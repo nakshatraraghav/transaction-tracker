4227cfd53d8b5d6b0fdd075b9628a0e151ef2807
Nakshatra Raghav <combatrickshaw@gmail.com>
2022-09-17 18:59:34 +0530

    added form validation

--- src/components/TransactionForm/TransactionForm.jsx
+++ src/components/TransactionForm/TransactionForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useRef } from "react";
 import { TransactionContext } from "../../context/TransactionContext/TransactionContext";
 
 import "./TransactionForm.css";
@@ -7,15 +7,31 @@ const TransactionForm = () => {
   const { addTransaction } = useContext(TransactionContext);
   const [text, setText] = useState();
   const [amount, setAmount] = useState();
+  const textRef = useRef();
+  const amountRef = useRef();
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (formValidation(text, amount)) {
+      const newTransaction = {
+        id: Math.floor(Math.random() * 100000000),
+        text: text,
+        amount: amount,
+      };
+      addTransaction(newTransaction);
+    } else {
+      textRef.current.placeholder = "Enter the Description";
+      amountRef.current.placeholder = "Enter the Amount";
+    }
+  };
 
-    const newTransaction = {
-      id: Math.floor(Math.random() * 100000000),
-      text: text,
-      amount: amount,
-    };
-    addTransaction(newTransaction);
+  const formValidation = (text, amount) => {
+    if (text === undefined) {
+      return false;
+    } else if (amount === undefined) {
+      return false;
+    } else {
+      return true;
+    }
   };
 
   return (
@@ -27,6 +43,7 @@ const TransactionForm = () => {
         </p>
         <input
           type="text"
+          ref={textRef}
           onChange={(event) => {
             setText(event.target.value);
           }}
@@ -38,6 +55,7 @@ const TransactionForm = () => {
         <p className="input-information">{"(+ve => Income, -ve => Expense)"}</p>
         <input
           type="number"
+          ref={amountRef}
           onChange={(event) => {
             setAmount(+event.target.value);
           }}
